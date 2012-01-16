10c39a6a4f79e0c3b966e5b032491653f67724a5
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-01-16 15:32:25 +0300

    add portaudio binary packages url, remove alexandria depends from package

--- src/package.lisp
+++ src/package.lisp
@@ -1,8 +1,12 @@
 (defpackage portaudio
-  (:use :cl :cffi :alexandria :ffa)
-  (:nicknames #:pa)
+  (:use :cl :cffi :ffa)
+  (:nicknames :pa)
   (:documentation
    "This package contains bindings to @a[http://portaudio.com/]{PortAudio}. PortAudio is a free, cross-platform, open-source, audio I/O library.
+
+Binary PortAudio packages can be founded here: 
+@a{http://planet.plt-scheme.org/display.ss?package=portaudio.plt&owner=clements} -> source browse -> directories lib
+
 @begin[Installation and Usage]{section}
 @begin{pre}
 git clone --depth 1 
