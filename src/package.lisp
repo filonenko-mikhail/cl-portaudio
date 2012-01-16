60c59fd810919238be932ffd92253bed0e021eda
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-01-16 15:33:59 +0300

    add git-clone url

--- src/package.lisp
+++ src/package.lisp
@@ -9,7 +9,7 @@ Binary PortAudio packages can be founded here:
 
 @begin[Installation and Usage]{section}
 @begin{pre}
-git clone --depth 1 
+git clone --depth 1 https://github.com/filonenko-mikhail/cl-portaudio.git
 emacs
 M+x slime
   (pushnew \"path/to/cl-portaudio\" asdf:*central-registry*)
